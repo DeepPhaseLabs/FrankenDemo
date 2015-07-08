989c0e13b248b6cc6f478c1ff6dcc7d0e3d13004
littleguair <littleguair@gmail.com>
2015-07-08 12:40:27 -0400

    Scripts

---
+++ Assets/Cardboard/Scripts/BehaviorScript/BehaviorScript/Program.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleBehaviourScript : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.renderer.material.color = Color.red;
+        }
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            gameObject.renderer.material.color = Color.green;
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            gameObject.renderer.material.color = Color.blue;
+        }
+    }
+}
+
+
+}
---
+++ Assets/Cardboard/Scripts/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,11 @@
+﻿var newVertices : Vector3[];
+var newUV : Vector2[];
+var newTriangles : int[];
+
+function Start () {
+	var mesh : Mesh = new Mesh ();
+	GetComponent.<MeshFilter>().mesh = mesh;
+	mesh.vertices = newVertices;
+	mesh.uv = newUV;
+	mesh.triangles = newTriangles;
+}
